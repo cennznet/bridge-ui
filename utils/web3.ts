9f84c29804de33d75fe82f66d74c81bad3d6f5cb
aidan-starke <starke.aidan@gmail.com>
2021-11-08 08:36:16 +1300

    get bridge contract

--- utils/web3.ts
+++ utils/web3.ts
@@ -1,9 +1,11 @@
+import CENNZnetBridge from "../artifacts/CENNZnetBridge.json";
 import ERC20Peg from "../artifacts/ERC20Peg.json";
 import TestToken from "../artifacts/TestToken.json";
 import { ethers } from "ethers";
 
-const ERC20PegAddress = "0x8E3E3f855d0A287DeF9b75a5a8463a61b720Bc26";
-const TokenAddress = "0x9644a0f31489effd3d5dcDb969a3629261662D1F";
+const BridgeAddress = "0x534e73B3dA8459d4f1519820DE67E5aBcf3F622F";
+const ERC20PegAddress = "0x1b54B0b559a1eDd89CBE77e0939f8D89E32A4904";
+const TokenAddress = "0xeDd17d870b7b02B2f2D59B17644eDF03ad72c41e";
 
 function initWeb3() {
   return new Promise(async (resolve, reject) => {
@@ -21,6 +23,12 @@ function initWeb3() {
       const provider = new ethers.providers.Web3Provider(ethereum);
       const signer = provider.getSigner();
 
+      const bridge: ethers.Contract = new ethers.Contract(
+        BridgeAddress,
+        CENNZnetBridge.abi,
+        signer
+      );
+
       const peg: ethers.Contract = new ethers.Contract(
         ERC20PegAddress,
         ERC20Peg.abi,
@@ -37,7 +45,7 @@ function initWeb3() {
         method: "eth_requestAccounts",
       });
 
-      resolve({ peg, token, accounts });
+      resolve({ bridge, peg, token, accounts });
     } catch (err) {
       reject(err);
     }
