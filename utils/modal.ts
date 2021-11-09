d71796465a5c160468042f5bd4527c168ef147d4
aidan-starke <starke.aidan@gmail.com>
2021-11-10 11:55:18 +1300

    add more states

--- utils/modal.ts
+++ utils/modal.ts
@@ -16,6 +16,15 @@ function defineModal(state: string, hash: string, setModalOpen: Function) {
     case "relayer":
       modal.text = "Your tokens will appear on CENNZnet soon!";
       break;
+    case "withdrawCENNZside":
+      modal.text = "Burning your tokens for withdrawal from CENNZnet...";
+      break;
+    case "withdrawETHside":
+      modal.text = "Withdrawing your tokens...";
+      break;
+    case "finished":
+      modal.text = "Done! You may now close this window";
+      break;
     case "error":
       if (hash === "noTokenSelected") modal.text = "Please select a token";
       else modal.text = "Whoops! Please try again";
