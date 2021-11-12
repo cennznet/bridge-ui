ef83e7754ebc1f1aa854304e75b5e0d565e52ad1
aidan-starke <starke.aidan@gmail.com>
2021-11-12 13:33:01 +1300

    withdrawCENNZ in withdraw component, refactor

--- pages/withdraw.tsx
+++ pages/withdraw.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { ethers } from "ethers";
 import { Box, Button, TextField } from "@mui/material";
 import TxModal from "../components/TxModal";
-import { useBlockchain } from "../context/blockchainContext";
-import { defineModal } from "../utils/modal";
-import { withdrawCENNZside } from "../utils/cennznet";
+import { useBlockchain } from "../context/BlockchainContext";
+import { defineTxModal } from "../utils/modal";
 import TokenPicker from "../components/TokenPicker";
+import { useWeb3 } from "../context/Web3Context";
 
 const Withdraw: React.FC<{}> = () => {
   const [customToken, setCustomToken] = useState(false);
@@ -18,24 +18,77 @@ const Withdraw: React.FC<{}> = () => {
     hash: "",
   });
   const { Contracts, Account }: any = useBlockchain();
+  const { signer, selectedAccount, api } = useWeb3();
 
   async function withdraw() {
     setModalOpen(false);
     if (token !== "") {
-      setModal(defineModal("withdrawCENNZside", "", setModalOpen));
+      setModal(defineTxModal("withdrawCENNZside", "", setModalOpen));
       let withdrawAmount = ethers.utils.parseUnits(amount).toString();
 
       const eventProof = await withdrawCENNZside(
         withdrawAmount,
         Account,
-        token,
-        Contracts.bridge
+        token
       );
       await withdrawEthSide(withdrawAmount, eventProof, Account, token);
       console.log("eventProof", eventProof);
     } else {
-      setModal(defineModal("error", "noTokenSelected", setModalOpen));
+      setModal(defineTxModal("error", "noTokenSelected", setModalOpen));
     }
+  }
+
+  async function withdrawCENNZside(
+    amount: any,
+    ethAddress: string,
+    tokenAddress: string
+  ) {
+    let eventProofId: any;
+    const tokenExist = await api.query.erc20Peg.erc20ToAssetId(tokenAddress);
+    const tokenId = tokenExist.isSome
+      ? tokenExist.unwrap()
+      : await api.query.genericAsset.nextAssetId();
+
+    await new Promise<void>((resolve) => {
+      api.tx.erc20Peg
+        .withdraw(tokenId, amount, ethAddress)
+        .signAndSend(
+          selectedAccount.address,
+          { signer },
+          async ({ status, events }: any) => {
+            if (status.isInBlock) {
+              for (const {
+                event: { method, section, data },
+              } of events) {
+                if (section === "erc20Peg" && method == "Erc20Withdraw") {
+                  eventProofId = data[0];
+                  console.log("*******************************************");
+                  console.log("Withdraw claim on CENNZnet side successfully");
+                  resolve();
+                }
+              }
+            }
+          }
+        );
+    });
+
+    let eventProof: any;
+    await new Promise<void>(async (resolve) => {
+      const unsubHeads = await api.rpc.chain.subscribeNewHeads(async () => {
+        console.log(`Waiting till event proof is fetched....`);
+        const versionedEventProof = (
+          await api.rpc.ethy.getEventProof(eventProofId)
+        ).toJSON();
+        if (versionedEventProof !== null) {
+          eventProof = versionedEventProof.EventProof;
+          console.log("Event proof found;::", eventProof);
+          unsubHeads();
+          resolve();
+        }
+      });
+    });
+
+    return eventProof;
   }
 
   async function withdrawEthSide(
@@ -77,9 +130,9 @@ const Withdraw: React.FC<{}> = () => {
       }
     );
 
-    setModal(defineModal("withdrawETHside", tx.hash, setModalOpen));
+    setModal(defineTxModal("withdrawETHside", tx.hash, setModalOpen));
     await tx.wait();
-    setModal(defineModal("finished", "", setModalOpen));
+    setModal(defineTxModal("finished", "", setModalOpen));
   }
 
   return (
@@ -103,10 +156,8 @@ const Withdraw: React.FC<{}> = () => {
           border: "3px outset #cfcfcf",
         }}
       >
-        {!customToken && <TokenPicker setToken={setToken} />}
-        {customToken && (
+        {customToken ? (
           <TextField
-            id="amount"
             label="Token Address"
             variant="filled"
             required
@@ -118,10 +169,26 @@ const Withdraw: React.FC<{}> = () => {
             }}
             onChange={(e) => setToken(e.target.value)}
           />
+        ) : (
+          <>
+            <TokenPicker setToken={setToken} />
+            <Button
+              size="small"
+              variant="outlined"
+              onClick={() => setCustomToken(true)}
+              sx={{
+                margin: "15px auto",
+                width: "70%",
+                display: "flex",
+                color: "secondary.dark",
+              }}
+            >
+              use token address
+            </Button>
+          </>
         )}
 
         <TextField
-          id="amount"
           label="Amount"
           variant="filled"
           required
@@ -141,21 +208,6 @@ const Withdraw: React.FC<{}> = () => {
           Withdraw
         </Button>
       </Box>
-      {!customToken && (
-        <Button
-          size="small"
-          variant="outlined"
-          onClick={() => setCustomToken(true)}
-          sx={{
-            margin: "15px auto",
-            width: "25%",
-            display: "flex",
-            color: "secondary.dark",
-          }}
-        >
-          use token address instead
-        </Button>
-      )}
     </>
   );
 };
