89e1b3daf30b4e4c0dc483f9af576a8d4a8a680c
aidan-starke <starke.aidan@gmail.com>
2021-11-12 13:33:21 +1300

    show accounts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { useBlockchain } from "../context/BlockchainContext";
+import { useWeb3 } from "../context/Web3Context";
+import CENNZnetWallet from "../components/CENNZnetWallet";
 
 const Home: React.FC<{}> = () => {
-  return <div></div>;
+  const { Account } = useBlockchain();
+  const { connectWallet, accounts, selectedAccount } = useWeb3();
+
+  async function connectCENNZwallet() {
+    await connectWallet();
+  }
+
+  return (
+    <>
+      <Box
+        sx={{
+          margin: "0 auto",
+          borderRadius: 10,
+          width: "30%",
+          height: "auto",
+          display: "block",
+          border: "3px outset #cfcfcf",
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{
+            textAlign: "center",
+            color: "secondary.dark",
+            textDecoration: "overline",
+            marginBottom: "10px",
+          }}
+        >
+          Wallets
+        </Typography>
+        <Typography
+          variant="h6"
+          sx={{
+            textAlign: "center",
+            color: "secondary.dark",
+          }}
+        >
+          Metamask:{" "}
+          {Account.substr(0, 6).concat(
+            "...",
+            Account.substr(Account.length - 4, 4)
+          )}
+        </Typography>
+        {!accounts && (
+          <Button
+            size="small"
+            variant="outlined"
+            sx={{
+              margin: "15px auto",
+              width: "70%",
+              display: "flex",
+              color: "secondary.dark",
+            }}
+            onClick={connectCENNZwallet}
+          >
+            Connect CENNZnet
+          </Button>
+        )}
+        <CENNZnetWallet />
+      </Box>
+    </>
+  );
 };
 
 export default Home;
