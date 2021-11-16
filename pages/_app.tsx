e23d6d8facac9d6e04c887d9d1e6e2cd605d3843
aidan-starke <starke.aidan@gmail.com>
2021-11-17 12:23:21 +1300

    display MetaMask connected network

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -9,33 +9,11 @@ import theme from "../components/theme";
 import BlockchainProvider from "../context/BlockchainContext";
 import { AppBar, Typography } from "@mui/material";
 import dynamic from "next/dynamic";
-import store from "store";
 
 const Web3 = dynamic(() => import("../components/Web3"), { ssr: false });
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-
-  useEffect(() => {
-    (async () => {
-      const { ethereum } = window as any;
-
-      let chainId = await ethereum.request({ method: "eth_chainId" });
-      console.log("Connected to chain " + chainId);
-
-      const ethChainId = store.get("eth-chain-id");
-      const ethNetwork = store.get("ethereum-network");
-
-      if (chainId !== ethChainId)
-        ethNetwork === "Ethereum"
-          ? alert("Please connect to Ethereum Mainnet!")
-          : alert(`Please connect to ${ethNetwork} Test Network!`);
-      else
-        await ethereum.request({
-          method: "eth_requestAccounts",
-        });
-    })();
-  }, []);
 
   return (
     <>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ const Home: React.FC<{}> = () => {
   const { Account } = useBlockchain();
   const [modalOpen, setModalOpen] = useState(false);
   const [currentNetwork, setCurrentNetwork] = useState("");
+  const [metamaskNetwork, setMetamaskNetwork] = useState("");
 
   useEffect(() => {
     const ethereumNetwork = store.get("ethereum-network");
@@ -25,6 +26,26 @@ const Home: React.FC<{}> = () => {
       default:
         break;
     }
+
+    (async () => {
+      const { ethereum }: any = window;
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+
+      switch (chainId) {
+        case "0x3":
+          setMetamaskNetwork("Ropsten");
+          break;
+        case "0x1":
+          setMetamaskNetwork("Mainnet");
+          break;
+        case "0x2a":
+          setMetamaskNetwork("Kovan");
+          break;
+        default:
+          setMetamaskNetwork(chainId);
+          break;
+      }
+    })();
   }, []);
 
   return (
@@ -89,7 +110,7 @@ const Home: React.FC<{}> = () => {
           sx={{
             textAlign: "center",
             color: "secondary.dark",
-            margin: "5px 0 10px",
+            margin: "5px 0",
           }}
         >
           Wallets
@@ -101,11 +122,20 @@ const Home: React.FC<{}> = () => {
             color: "secondary.dark",
           }}
         >
-          MetaMask:{" "}
+          MetaMask:
+          <br />
           {Account.substr(0, 6).concat(
             "...",
             Account.substr(Account.length - 4, 4)
           )}
+        </Typography>
+        <Typography
+          sx={{
+            textAlign: "center",
+            color: "secondary.dark",
+          }}
+        >
+          Connected to network: {metamaskNetwork}
         </Typography>
         <CENNZnetWallet />
       </Box>
