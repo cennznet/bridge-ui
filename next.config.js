6be2aa8836d634ff8e433997fd0f8efaaed77295
aidan-starke <starke.aidan@gmail.com>
2021-11-22 09:17:40 +1300

    add Teko font

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: "asset/resource",
+    });
+    return config;
+  },
+};
---
+++ theme/theme.ts
@@ -0,0 +1,49 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1130FF",
+    },
+    secondary: {
+      main: "#B3BDFF",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#B3BDFF",
+    },
+  },
+  typography: {
+    fontFamily: ["Roboto", "Teko"].join(","),
+  },
+  shape: {
+    borderRadius: 0,
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+      @font-face {
+        font-family: "Teko";
+        src: url("/fonts/Teko-Regular.ttf");
+        font-style: normal;
+        font-weight: 400;
+        font-display: swap;
+        src: local('Teko'), local('Teko-Regular'), url('/fonts/Teko-Regular.ttf') format('ttf');
+      }
+      
+      @font-face {
+        font-family: "Teko";
+        src: url("/fonts/Teko-Bold.ttf");
+        font-style: bold;
+        font-weight: 700;
+        font-display: swap;
+        src: local('Teko'), local('Teko-Bold'), url('/fonts/Teko-Bold.ttf') format('ttf');
+      }`,
+    },
+  },
+});
+
+export default theme;
