fe6d7854b11b939e2081c297f3a3a5becc14b432
aidan-starke <starke.aidan@gmail.com>
2021-11-25 09:22:16 +1300

    remove metadata update (chrome extension updated)

--- components/Web3.tsx
+++ components/Web3.tsx
@@ -11,53 +11,10 @@ import { Api as ApiPromise } from "@cennznet/api";
 import { hexToString } from "@polkadot/util";
 import { decodeAddress } from "@polkadot/keyring";
 import store from "store";
-import axios from "axios";
 import Web3Context from "../context/Web3Context";
 const EXTENSION = "cennznet-extension";
 import ERC20Tokens from "../artifacts/erc20tokens.json";
 import ErrorModal from "./ErrorModal";
-
-async function extractMeta(api) {
-  const systemChain = await api.rpc.system.chain();
-  const genesisHashExpected = api.genesisHash.toString();
-  const response = await axios.get(
-    "https://raw.githubusercontent.com/cennznet/api.js/master/extension-releases/runtimeModuleTypes.json"
-  );
-  const { data } = response;
-  const additionalTypes = data;
-  if (additionalTypes) {
-    let typesForCurrentChain = additionalTypes[genesisHashExpected];
-    // if not able to find types, take the first element (in case of local node the genesis Hash keep changing)
-    typesForCurrentChain =
-      typesForCurrentChain === undefined
-        ? Object.values(additionalTypes)[0]
-        : typesForCurrentChain;
-    let specTypes, userExtensions;
-    if (typesForCurrentChain) {
-      specTypes = typesForCurrentChain.types;
-      userExtensions = typesForCurrentChain.userExtensions;
-    }
-    const DEFAULT_SS58 = api.registry.createType("u32", addressDefaults.prefix);
-    const DEFAULT_DECIMALS = api.registry.createType("u32", 4);
-    const metadata = {
-      chain: systemChain,
-      color: "#191a2e",
-      genesisHash: api.genesisHash.toHex(),
-      icon: "CENNZnet",
-      metaCalls: Buffer.from(api.runtimeMetadata.asCallsOnly.toU8a()).toString(
-        "base64"
-      ),
-      specVersion: api.runtimeVersion.specVersion.toNumber(),
-      ss58Format: DEFAULT_SS58.toNumber(),
-      tokenDecimals: DEFAULT_DECIMALS.toNumber(),
-      tokenSymbol: "CENNZ",
-      types: specTypes,
-      userExtensions: userExtensions,
-    };
-    return metadata;
-  }
-  return null;
-}
 
 const Web3: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
   const [hasWeb3injected, setHasWeb3Injected] = useState(false);
@@ -155,20 +112,6 @@ const Web3: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
       );
 
       if (!cennznetWallet) throw new Error("CENNZnet wallet not found");
-      const metadata = cennznetWallet.metadata;
-      const checkIfMetaUpdated = localStorage.getItem(`EXTENSION_META_UPDATED`);
-      if (!checkIfMetaUpdated && api) {
-        try {
-          const metadataDef = await extractMeta(api);
-          if (metadataDef) {
-            await metadata.provide(metadataDef as any);
-            localStorage.setItem(`EXTENSION_META_UPDATED`, "true");
-          }
-        } catch (e) {
-          // any issue with metadata update should not ask to install extension
-          console.log(`update metadata rejected ${e}`);
-        }
-      }
 
       setWallet(cennznetWallet);
       store.set("CENNZNET-EXTENSION", cennznetWallet);
