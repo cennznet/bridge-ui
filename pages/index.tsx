c0f7989a6baa5d8a271e550c777893888589047b
aidan-starke <starke.aidan@gmail.com>
2021-11-30 07:42:14 +1300

    select CENNZnet account on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { Frame, Heading, SmallText } from "../components/StyledComponents";
 import { useRouter } from "next/router";
 import { useWeb3 } from "../context/Web3Context";
 import ErrorModal from "../components/ErrorModal";
+import WalletModal from "../components/WalletModal";
 
 const Home: React.FC<{}> = () => {
   const router = useRouter();
@@ -44,6 +45,12 @@ const Home: React.FC<{}> = () => {
     }
   };
 
+  const connectCENNZnet = () => {
+    connectWallet();
+    setModalState("changeAccount");
+    setModalOpen(true);
+  };
+
   const CENNZnetButton = (
     <Frame
       sx={{
@@ -53,7 +60,7 @@ const Home: React.FC<{}> = () => {
         height: "60px",
         width: "70%",
       }}
-      onClick={() => connectWallet()}
+      onClick={connectCENNZnet}
     >
       {selectedAccount ? (
         <>
@@ -144,9 +151,19 @@ const Home: React.FC<{}> = () => {
 
   return (
     <>
-      {modalOpen && (
-        <ErrorModal setModalOpen={setModalOpen} modalState={modalState} />
-      )}
+      {modalOpen &&
+        modalState !== "showWallet" &&
+        modalState !== "changeAccount" && (
+          <ErrorModal setModalOpen={setModalOpen} modalState={modalState} />
+        )}
+      {modalOpen &&
+        (modalState === "showWallet" || modalState === "changeAccount") && (
+          <WalletModal
+            modalState={modalState}
+            setModalOpen={setModalOpen}
+            setModalState={setModalState}
+          />
+        )}
       <Box
         sx={{
           position: "absolute",
