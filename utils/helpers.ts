482dff9ff236fd21b5cc3dd2c9c016d5b4454de1
aidan-starke <starke.aidan@gmail.com>
2021-11-30 10:57:45 +1300

    format balance with token decimals

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,33 +1,26 @@
 import { ethers } from "ethers";
+import GenericERC20TokenAbi from "../artifacts/GenericERC20Token.json";
 
-const ETH = "0x0000000000000000000000000000000000000000";
-const minABI = [
-  // balanceOf
-  {
-    constant: true,
-    inputs: [{ name: "_owner", type: "address" }],
-    name: "balanceOf",
-    outputs: [{ name: "balance", type: "uint256" }],
-    type: "function",
-  },
-];
+export const ETH = "0x0000000000000000000000000000000000000000";
 
 export const getMetamaskBalance = async (ethereum, tokenAddress, account) => {
   const provider = new ethers.providers.Web3Provider(ethereum);
   const signer = provider.getSigner();
-  let balance;
+  let balance, decimals;
 
   if (tokenAddress === ETH) {
     balance = await provider.getBalance(account);
   } else {
     const token: ethers.Contract = new ethers.Contract(
       tokenAddress,
-      minABI,
+      GenericERC20TokenAbi,
       signer
     );
+
+    decimals = await token.decimals();
 
     balance = await token.balanceOf(account);
   }
 
-  return Number(ethers.utils.formatEther(balance));
+  return Number(ethers.utils.formatUnits(balance, decimals));
 };
