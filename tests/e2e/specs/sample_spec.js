5ae4aa0e6be2cab653a1b4b2d4f8c733c819b200
aidan-starke <starke.aidan@gmail.com>
2021-12-02 10:12:07 +1300

    setup metamask at start of testing

--- tests/e2e/specs/sample_spec.js
+++ tests/e2e/specs/sample_spec.js
@@ -1,7 +1,16 @@
 describe("My First Test", () => {
-  it('finds the content "type"', () => {
+  it(`should setup metamask using private key`, () => {
+    cy.setupMetamask(Cypress.env("PRIVATE_KEY"), "kovan", "Tester@1234").then(
+      (setupFinished) => {
+        expect(setupFinished).to.be.true;
+      }
+    );
+  });
+  it(`should accept connection request to metamask`, () => {
     cy.visit("/");
-
     cy.contains("CONNECT METAMASK WALLET").click();
+    cy.acceptMetamaskAccess().then((connected) => {
+      expect(connected).to.be.true;
+    });
   });
 });
