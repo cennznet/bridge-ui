041492e50206c5e45b1e5c024e7650336fa94b57
aidan-starke <starke.aidan@gmail.com>
2021-12-03 12:01:47 +1300

    move setupMetamask to before hook

--- tests/e2e/specs/e2e_spec.js
+++ tests/e2e/specs/e2e_spec.js
@@ -1,38 +1,24 @@
-describe(
-  "e2e",
-  {
-    env: {
-      METAMASK_PRIVATE_KEY:
-        "c4ddfbf9541226e000c64e6d21ec2aa92c08aa6d75d099b6f5978dbc9874efb7",
-    },
-  },
-  () => {
-    before(() => {});
-    it(`should setup metamask using private key`, () => {
-      cy.setupMetamask(
-        Cypress.env("METAMASK_PRIVATE_KEY"),
-        "kovan",
-        "Tester@1234"
-      ).then((setupFinished) => {
-        expect(setupFinished).to.be.true;
-      });
+describe("e2e", () => {
+  before(() => {
+    cy.setupMetamask(
+      "c4ddfbf9541226e000c64e6d21ec2aa92c08aa6d75d099b6f5978dbc9874efb7",
+      "kovan",
+      "Tester@1234"
+    );
+  });
+  it(`should accept connection request to metamask`, () => {
+    cy.visit("/");
+    cy.get("[id='metamask-button']").click();
+    cy.acceptMetamaskAccess().then((connected) => {
+      expect(connected).to.be.true;
     });
-    it(`should accept connection request to metamask`, () => {
-      cy.visit("/");
-      cy.get("[id='metamask-button']").click();
-      cy.acceptMetamaskAccess().then((connected) => {
-        expect(connected).to.be.true;
-      });
 
-      cy.get("[id='metamask-button']").should("contain", "METAMASK");
-      cy.get("[id='metamask-button']").should("contain", "...");
-    });
-    it("should accept connection request to CENNZnet", () => {
-      cy.get("[id='cennznet-button']").click();
+    cy.get("[id='metamask-button']").should("contain", "METAMASK");
+    cy.get("[id='metamask-button']").should("contain", "...");
+  });
+  it("should accept connection request to CENNZnet", () => {
+    cy.get("[id='cennznet-button']").click();
 
-      cy.pause();
-    });
-  }
-);
-
-export {};
+    cy.pause();
+  });
+});
