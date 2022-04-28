fdca23ad55d4f0856471e918073140211a2a5a16
Aidan Starke <52016903+aidan-starke@users.noreply.github.com>
2022-04-28 18:09:39 +1200

    redirect to `app.cennz.net/bridge` (#51)
    * redirect to `app.cennz.net`
    
    * resolve `@types/react` to v17.0.2
    
    * redirect directly to bridge

--- next.config.js
+++ next.config.js
@@ -4,8 +4,22 @@ module.exports = {
 	webpack: (config) => {
 		config.module.rules.push({
 			test: /\.(woff|woff2|eot|ttf|otf)$/i,
-			type: "asset/resource",
+			type: "asset/resource"
 		});
 		return config;
 	},
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "https://app.cennz.net/bridge",
+				permanent: true
+			},
+			{
+				source: "/:path",
+				destination: "https://app.cennz.net/bridge",
+				permanent: true
+			}
+		];
+	}
 };
